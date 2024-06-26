97d11109abaa0fca7bb5f7c7637f725a63a8e4c7
dimarito <yashindmitry5@gmail.com>
2024-06-26 21:07:31 +0300

    Delete homework5.py

--- homework5.py
+++
@@ -1,9 +0,0 @@
-immutable_var = (12, False, 'Lesson')
-
-print(immutable_var)
-
-mutable_list = [[0, 0], [0, 1], [1, 0], [1, 1]]
-mutable_list[0][1] = 1
-mutable_list[1][1] = 0
-
-print(mutable_list)
