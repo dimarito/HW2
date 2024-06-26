c5840af413db09cafdcdc712793f788aca2fe5d0
dimarito <yashindmitry5@gmail.com>
2024-06-26 18:42:57 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_hw = 12
+number_hours = 1.5
+name_course = 'Python'
+one_time = number_hours / number_hw
+
+print(f'{name_course}, всего задач: {number_hw}, затрачено часов: {number_hours}, среднее время выполнения {one_time} часа.')
